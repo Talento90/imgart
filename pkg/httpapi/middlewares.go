d9740d12a2ebdf51c39bd642588d339713172803
Marco Talento <marcotalento90@gmail.com>
2017-12-18 22:55:43 +0000

    working on error handling

--- pkg/httpapi/middlewares.go
+++ pkg/httpapi/middlewares.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/talento90/gorpo/pkg/gorpo"
+
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -18,6 +20,28 @@ func response(statusCode int, body interface{}) appResponse {
 	return appResponse{
 		StatusCode: statusCode,
 		Body:       body,
+	}
+}
+
+func getHTTPError(err error) int {
+	if appErr, ok := err.(gorpo.Error); ok {
+
+		if gorpo.IsNotExists(appErr) {
+			return http.StatusNotFound
+		}
+
+		if gorpo.IsEValidation(appErr) {
+			return http.StatusUnprocessableEntity
+		}
+	}
+
+	return http.StatusInternalServerError
+}
+
+func errResponse(err error) appResponse {
+	return appResponse{
+		StatusCode: getHTTPError(err),
+		Body:       err,
 	}
 }
 
