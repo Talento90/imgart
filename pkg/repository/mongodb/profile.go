e52d6d5294bf3665e27ccc6f1aa5e19a641854e8
Marco Talento <marcotalento90@gmail.com>
2018-01-11 16:39:02 +0000

    working on profile

--- pkg/repository/mongodb/profile.go
+++ pkg/repository/mongodb/profile.go
@@ -17,26 +17,43 @@ func NewProfile(db *mgo.Database) gorpo.ProfileRepository {
 	}
 }
 
-func (r *profileRepository) GetAll() ([]gorpo.Profile, error) {
-	profiles := new([]gorpo.Profile)
+func (r *profileRepository) GetAll(limit int, skip int) (*[]gorpo.Profile, error) {
+	profiles := make([]gorpo.Profile, 0, limit)
 
-	err := r.collection.Find(bson.M{}).Skip(5).Limit(5).All(profiles)
+	err := r.collection.Find(bson.M{}).Skip(skip).Limit(limit).All(profiles)
 
-	return profiles, err
+	return &profiles, err
 }
 
-func (r *profileRepository) Get(id string) (Profile, error) {
+func (r *profileRepository) Get(id string) (*gorpo.Profile, error) {
+	profile := &gorpo.Profile{}
 
+	err := r.collection.FindId(id).One(profile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return profile, nil
 }
 
-func (r *profileRepository) Create(profile Profile) (Profile, error) {
+func (r *profileRepository) Create(profile gorpo.Profile) (*gorpo.Profile, error) {
+	err := r.collection.Insert(profile)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return &profile, nil
 }
 
 func (r *profileRepository) Update(profile Profile) error {
 
+	r.collection.Update(mgo.M)
 }
 
 func (r *profileRepository) Delete(id string) error {
+	err := r.collection.RemoveId(id)
 
+	return err
 }
